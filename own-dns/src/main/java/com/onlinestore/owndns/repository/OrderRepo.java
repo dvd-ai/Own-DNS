94f6510e4466a5816d39178c3bf3cd26dd8b6167
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2021-09-16 17:47:31 +0300

    order repo v1 added annot

--- own-dns/src/main/java/com/onlinestore/owndns/repository/OrderRepo.java
+++ own-dns/src/main/java/com/onlinestore/owndns/repository/OrderRepo.java
@@ -2,12 +2,13 @@ package com.onlinestore.owndns.repository;
 
 import com.onlinestore.owndns.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**v1
  * */
-
+@Repository
 public interface OrderRepo extends JpaRepository<Order, Long> {
 
     Optional<Order> findOrderById(Long id);
