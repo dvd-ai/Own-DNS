c88f9f54f387ca01e0c7ac12c8ecbe7f84e10b88
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2021-09-16 18:05:51 +0300

    v1

---
+++ own-dns/src/main/java/com/onlinestore/owndns/exception/OrderNotFoundException.java
@@ -0,0 +1,10 @@
+package com.onlinestore.owndns.exception;
+
+/**v1*/
+
+public class OrderNotFoundException extends RuntimeException{
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
