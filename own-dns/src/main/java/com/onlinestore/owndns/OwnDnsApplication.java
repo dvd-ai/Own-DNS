5edff58ac8cab299e28ac60da15b380c3a35db85
dvd-ai <56834006+dvd-ai@users.noreply.github.com>
2021-09-16 12:55:20 +0300

    spring initializr

---
+++ own-dns/src/main/java/com/onlinestore/owndns/OwnDnsApplication.java
@@ -0,0 +1,13 @@
+package com.onlinestore.owndns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnDnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnDnsApplication.class, args);
+	}
+
+}
---
+++ own-dns/src/test/java/com/onlinestore/owndns/OwnDnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinestore.owndns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnDnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
