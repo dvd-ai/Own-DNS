978e6664b389cb43eb0a1fd3841e79a1b52710cb
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2021-09-17 14:59:37 +0300

    SellerNotFoundException v1

---
+++ own-dns/src/main/java/com/onlinestore/owndns/exception/SellerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.onlinestore.owndns.exception;
+
+/**v1*/
+public class SellerNotFoundException extends RuntimeException {
+
+    public SellerNotFoundException(String message) {
+        super(message);
+    }
+}
