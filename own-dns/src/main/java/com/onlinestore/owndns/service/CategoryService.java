7c454b27a9ab6c4cf4c55886c661c3ff17bf0b4d
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2021-09-17 14:55:52 +0300

    CategoryService v1

---
+++ own-dns/src/main/java/com/onlinestore/owndns/service/CategoryService.java
@@ -0,0 +1,35 @@
+package com.onlinestore.owndns.service;
+
+import com.onlinestore.owndns.exception.CategoryNotFoundException;
+import com.onlinestore.owndns.model.Category;
+import com.onlinestore.owndns.repository.CategoryRepo;
+import org.springframework.stereotype.Service;
+
+/**v1*/
+@Service
+public class CategoryService {
+
+    private final CategoryRepo categoryRepo;
+
+
+    public CategoryService(CategoryRepo categoryRepo) {
+        this.categoryRepo = categoryRepo;
+    }
+
+    public Category addCategory(Category category) {
+        return categoryRepo.save(category);
+    }
+
+    public void deleteCategoryById(Long id) {
+        categoryRepo.deleteById(id);
+    }
+
+    public Category editCategory(Category category) {
+       return categoryRepo.save(category);
+    }
+
+    public Category findCategoryById(Long id) {
+        return categoryRepo.findCategoryById(id).orElseThrow(
+                ()-> new CategoryNotFoundException(getClass().getSimpleName() + " Category with id " + id + " not found!"));
+    }
+}
