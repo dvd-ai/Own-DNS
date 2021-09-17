2ee2aa4d26e35e9d82c9080676414a9b1add3e3f
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2021-09-17 13:48:27 +0300

    ReviewNotFoundException v1

---
+++ own-dns/src/main/java/com/onlinestore/owndns/exception/ReviewNotFoundException.java
@@ -0,0 +1,10 @@
+package com.onlinestore.owndns.exception;
+
+
+/**v1*/
+public class ReviewNotFoundException extends RuntimeException{
+
+    public ReviewNotFoundException(String message) {
+        super(message);
+    }
+}
