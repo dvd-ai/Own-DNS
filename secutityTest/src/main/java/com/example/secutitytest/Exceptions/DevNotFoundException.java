96f9fcad5e13f83681d752c9b7f6d36050c505cb
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2021-09-26 17:26:49 +0300

    upd

--- secutityTest/src/main/java/com/example/secutitytest/Exceptions/DevNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.secutitytest.Exceptions;
-
-public class DevNotFoundException extends RuntimeException{
-    public DevNotFoundException(String message){
-        super(message);
-
-    }
-}
